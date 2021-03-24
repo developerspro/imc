9589c3606d3dd7d015b0fb1b75bee9f5080bc8e1
jeferson.oferreira@etec.sp.gov.br <jeferson.oferreira@etec.sp.gov.br>
2021-03-23 22:32:32 -0300

    alteracao

--- calculaImc.php
+++ calculaImc.php
@@ -1,11 +1,11 @@
 <?php 
-
  $nome = $_POST['nome'];
  $peso = $_POST['peso'];
  $altura = $_POST['altura'];
  $idade = $_POST['idade'];
+ $idade = $_POST['idade'];
 
-include "Pessoa.php";
+ include "Pessoa.php";
 $paciente = new Pessoa();
 $paciente->nome = "Robin";
 $pes = new Pessoa();
